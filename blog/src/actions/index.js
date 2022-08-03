fafaf14fd3918a3c37f49a2156c6566c2c54a940
unknown <sazwerling@gmail.com>
2022-08-03 17:31:39 -0500

    start adding thunk middlware

---
+++ blog/src/actions/index.js
@@ -0,0 +1,24 @@
+import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch, getState) => {  //don't neeed getState arg becuase we're not using it
+        const response = await jsonPlaceholder.get('/posts');    // using arrow functions to get rid of brackets and return
+        dispatch({ type: 'FETCH_POSTS', payload: response})
+    }    
+
+
+
+
+
+//CANNOT DO THIS  // THE REASON FOR THUNK MIDDLEWARE
+// Because what gets returned is not a pure js object.
+// What actually gets returned is the response from jsonPlaceholder.get.
+// And in the time action gets to a reducer, we won't have fetched our data.
+
+// export const fetchPosts = async () => {
+//     const response = await jsonPlaceholder.get('/posts');
+
+//     return {
+//         type: `FETCH_POSTS`,
+//         payload: response
+//     };
+// };
