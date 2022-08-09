a121726f141c253fcc3ce4e9e5003eefb0a7d19e
unknown <sazwerling@gmail.com>
2022-08-09 17:46:39 -0500

    with some material ui styling

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -11,7 +11,15 @@ class PostList extends React.Component {
     renderList(){
         return this.props.posts.map(post => {
             return(
-                <div key={post.id}>{post.userId}: {post.title}</div>
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
             )
         })
     }
@@ -19,7 +27,7 @@ class PostList extends React.Component {
     render(){
         console.log(this.props.posts)
         return(
-            <div>
+            <div className="ui relaxed divided list">
                 {this.renderList()}
             </div>
         )
