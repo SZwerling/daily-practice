134e4ce186ce91c5a359d5b02739abf4e36e60fc
unknown <sazwerling@gmail.com>
2022-08-09 16:01:45 -0500

    posts reducer

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,7 +1,25 @@
-export default () => {
-    return 123;
+export default (state = [], action) => {
+    if(action.type === `FETCH_POSTS`){
+        return action.payload;
+    }
+    return state;
 };
 
 
 
-// reducer must return any values besides undefined
+// reducer cannot return undefined
+// produces 'state' or data using only previous data and the action
+// cannot reach out of itself to return data // no api req's
+// must not mutate its input state arg // no state.examp = "new thing" // or push, pop
+
+
+//  REMOVING an element from arr ---  state.filter(element => element !== "hi")
+//  ADDING an element to an arr --- [...state, 'hi']
+//  REPLACING an element in an arr --- state.map(element => element === "hi"? "bye" : element)
+// make new arr
+
+// OBJECTS
+// UPDATING property in an object --- {...state, name: "Sam"}
+// ADDING property to an object ---   {...state, age: 30}
+// REMOVING property from an object --- {...state, age: undedfined} // _.omit(state, 'age')
+// make new object
