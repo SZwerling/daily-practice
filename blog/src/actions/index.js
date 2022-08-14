2e7740f256860e0e365a7e07479c51738c02752c
unknown <sazwerling@gmail.com>
2022-08-13 22:33:13 -0500

    refactor fetchUser using lodash memoize to avoid overfetching

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch, getState) => {  //don't neeed getState arg becuase we're not using it
@@ -5,14 +6,24 @@ export const fetchPosts = () => async (dispatch, getState) => {  //don't neeed g
         dispatch({ type: 'FETCH_POSTS', payload: response.data}) // pulling off just the 'data' key/value from response object
     }   
     
-export const fetchUser = (id) => async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: 'FETCH_USER', payload: response.data})
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch)
 }
 
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data})
+});
 
 
 
+
+// FUNCTION BEFORE MEMOIZE // MEMOIZE MEANS THAT IF THE SAME FUNCTION IS CALLED WITH SAME INPUTS, 
+// IT ACTUALLY WILL NOT RUN AGAIN, JUST RETURN SAME OUTPUT, THANKS TO LODASH LIBRARY // NO OVERFETCHING
+// export const fetchUser = (id) => (dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data})
+// }
 
 //CANNOT DO THIS  // THE REASON FOR THUNK MIDDLEWARE
 // Because what gets returned is not a pure js object.
