2019373e6625a3fca0904b02b8164fb238ea6c86
unknown <sazwerling@gmail.com>
2022-09-24 11:48:21 -0700

    styling for search comp

--- meals-app/src/components/Search.js
+++ meals-app/src/components/Search.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useGlobalContext } from "../context";
+
 const Search = () => {
-    return(
-        <div>Search</div>
-    )
-}
+    const [text, setText] = useState('')
+    console.log('rerender')
+    function handleChange(term){
+        setText(term)
+    }
+    
+
+    function handleSubmit(e){
+        console.log(text)
+        e.preventDefault()
+    }
+
+   return (
+      <header className="search-container">
+         <form className="form" onSubmit={handleSubmit}>
+            <label htmlFor="search-input"></label>
+            <input onChange={e => handleChange(e.target.value)}
+               id="search-input"
+               type="text"
+               placeholder="type favorite meal"
+               className="form-input"
+            />
+            <button type="submit" className="btn">
+               search
+            </button>
+            <button type="button" className="btn btn-hipster">
+               suprise me !
+            </button>
+         </form>
+      </header>
+   );
+};
 
 export default Search;
